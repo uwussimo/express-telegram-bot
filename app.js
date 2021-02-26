3a51617bbbf5e34b3ef1e85fe1682f2ce02b09c1
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-02-26 13:50:05 +0000

    Minor changes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", () => {
-  app.render("index", {});
+app.get("/", (req, res) => {
+  res.send("success");
 });
 
 app.listen(3000, () => console.log("Server has been started"));
